34c51f48aa79e7cc396d9ae8c0da4b4bab5f4c8f
Rodrigo Amaral <rodrigoamaral@gmail.com>
2019-04-13 12:07:25 -0300

    Adiciona exercícios

--- exercicios/variaveis.py
+++ exercicios/variaveis.py
@@ -1,10 +1,16 @@
 x = 5
 y = 7.5
 z = x
+s = 'exemplo'
 x = y + 5
-y = z - 3
+y = z - 1
 z = x * 2
+t = s
+s = y // 2
+
 
 print('x =', x, type(x))
 print('y =', y, type(y))
-print('z =', x, type(z))
+print('z =', z, type(z))
+print('s =', s, type(s))
+print('t =', t, type(t))
